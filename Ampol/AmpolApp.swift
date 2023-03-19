6bae4c845c9704e12d456a670d6c1ad2602983e0
Dmitrii Zverev <dima.newmail@gmail.com>
2023-03-19 14:01:48 +1000

    Initial Commit

---
+++ Ampol/AmpolApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmpolApp.swift
+//  Ampol
+//
+//  Created by Dmitrii Zverev on 19/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct AmpolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
